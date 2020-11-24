2dee9e824a834789b2655c4ab450f2c95f48baa8
jerryrat <xrat@Foxmail.com>
2020-11-24 13:44:29 +0800

    Update waifu-tips.js

--- waifu-tips.js
+++ waifu-tips.js
@@ -21,13 +21,13 @@ function loadWidget(config) {
 			<div id="waifu-tips"></div>
 			<canvas id="live2d" width="800" height="800"></canvas>
 			<div id="waifu-tool">
-				<span class="fa fa-lg fa-comment"></span>
+				<!--span class="fa fa-lg fa-comment"></span-->
 				<span class="fa fa-lg fa-paper-plane"></span>
 				<span class="fa fa-lg fa-user-circle"></span>
 				<span class="fa fa-lg fa-street-view"></span>
 				<span class="fa fa-lg fa-camera-retro"></span>
 				<span class="fa fa-lg fa-info-circle"></span>
-				<span class="fa fa-lg fa-times"></span>
+				<span class="fa fa-lg fa-times"></span-->
 			</div>
 		</div>`);
 	// https://stackoverflow.com/questions/24148403/trigger-css-transition-on-appended-element
